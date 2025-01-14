a896c22a63635cf8e567f6959fd5dbeaf34da29b
Sai-S-Mc <sai.satti97@gmail.com>
2025-01-13 20:32:02 -0500

    Integrated AI API  and added API-call handling function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+function handleApiCall(response) {
+  console.log("API response received");
+  new Typewriter("#name", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 45,
+    cursor: null,
+  });
+}
+
 function handleFormSubmit(event) {
   event.preventDefault();
   let radioSelection = document.querySelector("input[name='gender']:checked");
@@ -5,20 +15,39 @@ function handleFormSubmit(event) {
 
   let cultureInputElement = document.querySelector("#culture");
   let cultureInput = cultureInputElement.value;
+  if (cultureInput.length > 0) {
+    cultureInput = `, based on ${cultureInput} culture`;
+  } else {
+    cultureInput = "";
+  }
 
   let lengthInputElement = document.querySelector("#length");
   let lengthInput = lengthInputElement.value;
+  if (lengthInput > 0 && lengthInput <= 10) {
+    lengthInput = ` that is exactly ${lengthInput} characters long(NOT including whitespace)`;
+  } else if (lengthInput > 10) {
+    lengthInput = 10;
+    lengthInput = ` that is exactly ${lengthInput} characters long(NOT including whitespace)`;
+  } else {
+    lengthInput = "";
+  }
 
-  new Typewriter("#name", {
-    strings: `A trendy name for a girl is Luna(LOO-nah).
-          <br />
-          Luna is of Latin origin, meaning "moon," symbolizing light, beauty,
-          and mystery.`,
-    autoStart: true,
-    delay: 55,
-    cursor: `<span class="material-symbols-outlined heart-cursor">favorite</span>`,
-  });
+  let apiKey = "tbfob32e017e01391b34fe15b81ad2a6";
+  let context =
+    "You are knowledgable in human names and know many names, based on different cultures and their meanings. You are especially talented in coming up with baby names based on user instructions.Make sure that the answer you generate is in HTML format as in the example. Here's an example: <div>A trendy name for a girl is Luna(LOO-nah).<br />Luna is of Latin origin, meaning 'moon', symbolizing light, beauty and mystery.</div> Please be respectful. Use appropriate emojis to make it fun.";
+  let prompt = `Please generate a ${genderSelection} name along with it's pronunciation right after${cultureInput}${lengthInput} and it's meaning in a short sentence.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(prompt);
+  console.log(context);
+  console.log("making an API call");
+
+  axios.get(apiUrl).then(handleApiCall);
 }
 
 let formElement = document.querySelector("#name-generator-form");
 formElement.addEventListener("submit", handleFormSubmit);
+
+// let testString = "hello how are you"
+// testString = testString.autocapitalize();
+// console.log(testString);
