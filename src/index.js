9c7966ffa8597d7db5de8a110e69b1e95c484bd1
Sai-S-Mc <sai.satti97@gmail.com>
2025-08-12 11:09:37 -0400

    Update JS
    - Add a catch to the API call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-function handleApiCall(response) {
-  console.log(response.data);
 
+function handleApiCall(response) {
   new Typewriter("#apiResponse", {
     strings: response.data.answer,
     autoStart: true,
@@ -44,20 +43,20 @@ function handleFormSubmit(event) {
   let lengthInput = lengthInputElement.value;
   let lengthContext = "";
   if (lengthInput) {
-    lengthContext = ` Make sure to obey the ${lengthInput} character requirement when it comes to the name not its pronunciation, if you cannot find a name that can obey the ${lengthInput} character requirement, include a short but playful apology and explanation before the rest of the answer in HTML format.For example, <div>I searched the solar system for a ${genderSelection} name that is exactly ${lengthInput} characters long but I couldn't find any. Here's a name that I think you might like. I counted it and I think it is [specify the character length] characters long (I am sometimes bad at Math).<br/><br/></div>`;
+    lengthContext = ` Make sure to obey the ${lengthInput} character requirement when it comes to the name not its pronunciation. If you cannot find a name that can obey the ${lengthInput} character requirement, include a short but playful apology and explanation before the rest of the answer in HTML format.For example, <div>I searched the solar system for a ${genderSelection} name that is exactly ${lengthInput} characters long but I couldn't find any. Here's a name that I think you might like. I counted it and I think it is [specify the character length] characters long (I am sometimes bad at Math).<br/><br/></div>`;
   }
 
   let optionalPreference = formatOptionalInputs(cultureInput, lengthInput);
 
   let apiKey = "tbfob32e017e01391b34fe15b81ad2a6";
-  let context = `You are knowledgable in human names and know many names, based on different cultures and their meanings. You are especially talented in coming up with baby names based on user instructions. Please generate different answers every time. Make sure that the answer you generate is in HTML format as in the example. Here's an example: <div>A trendy name for a girl is Luna(LOO-nah).<br />Luna is of Latin origin, meaning 'moon', symbolizing light, beauty and mystery.</div>${lengthContext} Use appropriate emojis to make it fun.`;
+  let context = `You are knowledgable in human names and know many names, based on different cultures and their meanings. You are especially talented in coming up with baby names based on user instructions. Please generate different answers every time. Make sure that the answer you generate is in HTML format as in the example. Here's an example: <div>A trendy name for a girl is Luna(LOO-nah).<br />Luna is of Latin origin, meaning 'moon', symbolizing light, beauty and mystery.</div>${lengthContext}.Please be respectful. Use appropriate emojis to make it fun.`;
   let prompt = `Please generate a ${genderSelection} name along with it's pronunciation and it's meaning in a short sentence.${optionalPreference}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let apiResponseElement = document.querySelector("#apiResponse");
   apiResponseElement.classList.remove("hidden");
   apiResponseElement.innerHTML = `<span class="generating">⏳Working my magic... Generating a name⏳</span>`;
-  axios.get(apiUrl).then(handleApiCall);
+  axios.get(apiUrl).then(handleApiCall).catch(handleError);
 }
 
 let formElement = document.querySelector("#name-generator-form");
